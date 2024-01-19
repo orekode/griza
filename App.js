210e6eefcd8f4cd3792c26a63546037cb6c518a1
Divine Osei <96095888+divi-nation@users.noreply.github.com>
2024-01-19 08:29:12 +0000

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./Navigations";
 import { QueryClient, QueryClientProvider } from 'react-query';
-
+  
 export default function App() {
   const queryClient = new QueryClient();
   return (
